98d8e083c72f074f66ab960cff147479816cfbc8
Matthew Pearson <matthew@Matthews-MacBook-Pro.local>
2020-02-13 13:29:43 -0500

    add dalinar and kaladin scripts

---
+++ dalinar.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+trap "kill 0" EXIT
+
+# generate i/o on the system
+while true
+do
+  cp loadfile loadfile1
+done
+
+wait
---
+++ kaladin.sh
@@ -0,0 +1,8 @@
+! /bin/bash
+
+trap "kill 0" EXIT
+
+# generate load on the system
+cat /dev/urandom | gzip -1 > /dev/null
+
+wait
